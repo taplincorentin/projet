7ffcc703d473e682c09c93e304f3f6b0c39138c1
Taplin Corentin <you@example.com>
2023-11-08 17:01:32 +0100

    chienform

---
+++ src/Controller/ChienController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Chien;
+use App\Form\ChienFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ChienController extends AbstractController
+{
+    // #[Route('/chien', name: 'app_chien')]
+    // public function index(): Response
+    // {
+    //     return $this->render('chien/index.html.twig', [
+    //         'controller_name' => 'ChienController',
+    //     ]);
+    // }
+
+    #[Route('/chien/new', name: 'new_chien')]
+    #[Route('/chien/{id}/edit', name: 'edit_chien')]
+    public function new_edit(Chien $chien = null, Request $request, EntityManagerInterface $entityManager): Response {
+        
+        if(!$chien) { //condition if no chien create new one otherwise it's an edit of the existing one
+            $chien = new Chien();
+        }
+
+        $form = $this->createForm(ChienFormType::class, $chien);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) { //if form submitted and valid
+            
+            $chien = $form->getData();
+            $entityManager->persist($chien); //prepare
+            $entityManager->flush(); //execute
+
+            return $this->redirectToRoute('app_chien'); //redirect to chienList
+
+        }
+        return $this->render('chien/new.html.twig', [
+            'formAddChien' => $form,
+        ]);
+
+    }
+}
