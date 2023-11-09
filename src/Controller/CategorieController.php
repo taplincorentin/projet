f1cf106308a9b4a4aea396a790972e155f954629
Corentin Taplin <taplincorentin@gmail.com>
2023-11-09 17:42:23 +0100

    categorylist and made show_category route

---
+++ src/Controller/CategorieController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Categorie;
+use App\Repository\CategorieRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CategorieController extends AbstractController
+{
+    #[Route('/categorie', name: 'app_categorie')]
+    public function index(CategorieRepository $categorieRepository): Response
+    {
+        $categories = $categorieRepository->findBy([], ['nom' => 'ASC']); //get all categories
+        
+        return $this->render('categorie/index.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
+
+
+    #[Route('/categorie/{id}', name: 'show_categorie')]
+    public function show(Categorie $categorie = null): Response {
+        if($categorie){
+            return $this->render('categorie/show.html.twig', [
+                'categorie' => $categorie
+            ]);
+        }
+        else {
+            return $this->redirectToRoute('app_categorie');
+        }
+    }
+
+}
--- src/Entity/Categorie.php
+++ src/Entity/Categorie.php
@@ -72,4 +72,8 @@ class Categorie
 
         return $this;
     }
+
+    public function __toString(){
+        return $this->nom;
+    }
 }
