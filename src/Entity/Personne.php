b5efc1e5745de6827a61a53e5e5dba923216d216
Corentin Taplin <taplincorentin@gmail.com>
2023-11-09 09:13:24 +0100

    added email attribute to user

--- src/Entity/Personne.php
+++ src/Entity/Personne.php
@@ -12,13 +12,17 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: PersonneRepository::class)]
-#[UniqueEntity(fields: ['pseudo'], message: 'pseudo déjà utilisé')] //check pseudo est unique
+#[UniqueEntity(fields: ['pseudo'], message: 'username already used')] //check pseudo est unique
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')] //check email est unique
 class Personne implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
+
+    #[ORM\Column(length: 255, unique: true)]
+    private ?string $email = null;
 
     #[ORM\Column(length: 180, unique: true)] 
     private ?string $pseudo = null;
@@ -52,6 +56,8 @@ class Personne implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\OneToMany(mappedBy: 'auteur', targetEntity: Post::class)]
     private Collection $posts;
+
+    
 
     public function __construct()
     {
@@ -264,6 +270,18 @@ class Personne implements UserInterface, PasswordAuthenticatedUserInterface
                 $post->setAuteur(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
 
         return $this;
     }
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -21,6 +22,7 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('email', EmailType::class, ['label' => 'Adresse e-mail :'])
             ->add('pseudo', TextType::class, ['label' => 'Pseudo : '])
             ->add('plainPassword', RepeatedType::class, [
                 'mapped' => false,
