5c1f0c39fdbcba93f02c5e46e3282c5ecb825e2e
Corentin Taplin <taplincorentin@gmail.com>
2023-11-09 11:48:42 +0100

    création des attributs fichier et nom image

--- src/Entity/Chien.php
+++ src/Entity/Chien.php
@@ -7,8 +7,11 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: ChienRepository::class)]
+#[Vich\Uploadable]
 class Chien
 {
     #[ORM\Id]
@@ -19,11 +22,20 @@ class Chien
     #[ORM\Column(length: 20)]
     private ?string $nom = null;
 
+    #[Vich\UploadableField(mapping: 'dchien_images', fileNameProperty: 'imageName')]
+    private ?File $imageFile = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?string $imageName = null;
+
     #[ORM\Column(type: Types::DATE_MUTABLE)]
     private ?\DateTimeInterface $dateNaissance = null;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
     private ?string $description = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTime $dateActualisation = null;
 
     #[ORM\ManyToOne(inversedBy: 'chiens')]
     #[ORM\JoinColumn(nullable: false)]
@@ -52,6 +64,34 @@ class Chien
         return $this;
     }
 
+
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(?string $imageName): void
+    {
+        $this->imageName = $imageName;
+    }
+
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
     public function getDateNaissance(): ?\DateTimeInterface
     {
         return $this->dateNaissance;
@@ -76,6 +116,19 @@ class Chien
         return $this;
     }
 
+    public function getDateActualisation()
+    {
+        return $this->dateActualisation;
+    }
+
+    public function setDateActualisation(\DateTime $dateActualisation): static
+    {
+        $this->dateActualisation = $dateActualisation;
+
+        return $this;
+    }
+
+
     public function getPersonne(): ?Personne
     {
         return $this->personne;
@@ -87,6 +140,7 @@ class Chien
 
         return $this;
     }
+
 
     public function __toString(){
         return $this->nom;
