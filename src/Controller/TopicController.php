ce72ee508a3b38c034310c36a62bdf330ca719eb
Taplin Corentin <you@example.com>
2023-11-10 13:50:40 +0100

    topic and post form type creation

--- src/Controller/TopicController.php
+++ src/Controller/TopicController.php
@@ -15,6 +15,48 @@ class TopicController extends AbstractController
         return $this->redirectToRoute('app_home');
     }
 
+
+    #[Route('/topic/new', name: 'new_topic')]
+    #[Route('/topic/{id}/edit', name: 'edit_topic')]
+    public function new(Topic $topic = null, Request $request, EntityManagerInterface $entityManager): Response {
+
+        if(!$topic) { //condition if no stagiaire create new one otherwise it's an edit of the existing one
+            $topic = new Topic();
+        }
+
+        $form = $this->createForm(TopicType::class, $topic);
+
+        $form->handleRequest($request); 
+        if ($form->isSubmitted() && $form->isValid()) { //if form submitted and valid
+            $topic = $form->getData();
+            $entityManager->persist($topic); //prepare
+            $entityManager->flush(); //execute
+
+            return $this->redirectToRoute('app_topic'); //redirect to topicList
+
+        }
+
+        return $this->render('topic/new.html.twig', [
+            'formAddTopic' => $form,
+        ]);
+
+    }
+
+
+    #[Route('/topic/{id}/delete', name: 'delete_topic')]
+
+    public function delete(Topic $topic, EntityManagerInterface $entityManager) {
+
+        $categorie=$topic->getCategorie();      //récupération de la catégorie pour la redirection
+        
+        $entityManager->remove($topic);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('show_categorie', ['id' => $categorie->getId()]);; //redirection page categorie du topic supprimé
+    }
+
+
+
     #[Route('/topic/{id}', name: 'show_topic')]
     public function show(Topic $topic = null): Response {
         if($topic){
@@ -23,7 +65,7 @@ class TopicController extends AbstractController
             ]);
         }
         else {
-            return $this->redirectToRoute('app_topic');
+            return $this->redirectToRoute('app_home');
         }
     }
 }
---
+++ src/Form/PostFormType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Post;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('contenu')
+            ->add('dateCreation')
+            ->add('topic')
+            ->add('auteur')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}
