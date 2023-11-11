50aa0cd3edb640a6397524f217d0a6ee8c40fa30
Corentin Taplin <taplincorentin@gmail.com>
2023-11-11 14:24:52 +0100

    creation service CallApiService

---
+++ src/Service/CallApiService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class CallApiService
+{
+
+    public function getBreedList(): array
+    {
+        
+    }
+
+}
