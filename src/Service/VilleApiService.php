8208e90642516a0df2cc6aa0cafec9e3b52d4670
Corentin <taplincorentin@gmail.com>
2023-11-13 17:15:18 +0100

    might need to use another api for city list

--- src/Service/VilleApiService.php
+++ src/Service/VilleApiService.php
@@ -15,7 +15,7 @@ class VilleApiService
 
     public function getVilleListe()
     {
-        $url = 'http://api.openweathermap.org/data/2.5/box/city?bbox=180,-90,180,90,100&appid=%s=6977d652c686df85875de405090c55f2';
+        $url = 'http://api.openweathermap.org/data/2.5/box/city?bbox=180,-90,180,90,100&appid=%s=';
 
         $response = $this->client->request('GET', $url);
         $data = $response->toArray();
