019a9a91c7b65203804d178b13181fdefbe75cf7
Corentin Taplin <taplincorentin@gmail.com>
2023-11-13 16:19:07 +0100

    modified error in ville api interface

--- src/Service/VilleApiService.php
+++ src/Service/VilleApiService.php
@@ -2,23 +2,22 @@
 
 namespace App\Service;
 
-use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class VilleApiService
 {
+    private $client;
 
-    private $httpClient;
-
-    public function __construct()
+    public function __construct(HttpClientInterface $client)
     {
-        $this->httpClient = HttpClient::create();
+        $this->client = $client;
     }
 
     public function getVilleListe()
     {
         $url = 'http://api.openweathermap.org/data/2.5/box/city?bbox=180,-90,180,90,100&appid=%s=6977d652c686df85875de405090c55f2';
 
-        $response = $this->httpClient->request('GET', $url);
+        $response = $this->client->request('GET', $url);
         $data = $response->toArray();
 
         $rawVilles = $data['list'];
