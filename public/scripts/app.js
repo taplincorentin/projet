0d80f272c668cbead18fdb8f11d6f4265006f4ce
Corentin <taplincorentin@gmail.com>
2023-11-16 12:16:24 +0100

    moved script in js file +hide input fields for latitude/longitude

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,25 +1,68 @@
-//chercher la liste de race de dog.ceo api
-// async function start(){
-//     const reponse = await fetch("https://dog.ceo/api/breeds/list/all")
-//     const data = await reponse.json()
-//     creationListeRaces(data.message)
-// }
+document.addEventListener('DOMContentLoaded', function () {
+    
+    //initialize map variable
+    var map = L.map('map_add_balade').setView([51.505, -0.09], 13);
 
-// start()
+    //get opensteetmap tile and add attribution
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
-//création du menu déroulant à partir de la liste de races
-// function creationListeRaces(listeRaces){
+    //initialize marker variable
+    var marker = L.marker([51.5, -0.09]).addTo(map);
 
-//     document.getElementById("race").innerHTML = `
-//         <select>
-//             <option>Choose a breed</option>
-//             ${Object.keys(listeRaces).map(function (race){
-//                 return `<option>${race}</option>`
-//                 }).join('')}
-//             }
-//         </select>
-//     `
-// }
+    //when click get get lat and long values
+    map.on('click', function(e) {
+        marker.setLatLng(e.latlng);
+        // Update input fields with latitude and longitude
+        document.getElementById('balade_form_pointLatitude').value = e.latlng.lat;
+        document.getElementById('balade_form_pointLongitude').value = e.latlng.lng;   
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    
+    //initialize map variable
+    var map = L.map('map_add_seance').setView([51.505, -0.09], 13);
+
+    //get opensteetmap tile and add attribution
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    //initialize marker variable
+    var marker = L.marker([51.5, -0.09]).addTo(map);
+
+    //when click get get lat and long values
+    map.on('click', function(e) {
+        marker.setLatLng(e.latlng);
+        // Update input fields with latitude and longitude
+        document.getElementById('seance_form_pointLatitude').value = e.latlng.lat;
+        document.getElementById('seance_form_pointLongitude').value = e.latlng.lng;   
+    });
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    //
+        var mapDiv = document.getElementById('map_show');
+        var latitude = parseFloat(mapDiv.dataset.latitude);
+        var longitude = parseFloat(mapDiv.dataset.longitude);
+
+
+        var map = L.map('map_show').setView([ latitude , longitude ], 12);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([ latitude , longitude ]).addTo(map);
+            
+});
+
+
 
 
 
