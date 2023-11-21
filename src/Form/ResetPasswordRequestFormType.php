64e25e602d41387a796d205bcd0b1ffa62f03e09
Corentin <taplincorentin@gmail.com>
2023-11-21 16:42:47 +0100

    css for reset-password pages

--- src/Form/ResetPasswordRequestFormType.php
+++ src/Form/ResetPasswordRequestFormType.php
@@ -3,10 +3,11 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ResetPasswordRequestFormType extends AbstractType
 {
@@ -20,7 +21,13 @@ class ResetPasswordRequestFormType extends AbstractType
                         'message' => 'Please enter your email',
                     ]),
                 ],
+                'label' => "Email"
             ])
+
+            ->add('submit', SubmitType::class, [
+                'label' => "Send password reset email",
+            ]);
+            
         ;
     }
 
