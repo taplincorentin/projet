33a1698d2cba8e41cdf0dd157ddc2d97d088e702
Corentin <taplincorentin@gmail.com>
2023-11-22 10:38:47 +0100

    redirection to from home to login if not logged in

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,14 @@ class HomeController extends AbstractController
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        $user->getUser();
+        if($user){
+            return $this->render('home/index.html.twig', [
+                'controller_name' => 'HomeController',
+            ]);
+        }
+
+        return $this->redirectToRoute('app_login');
+        
     }
 }
