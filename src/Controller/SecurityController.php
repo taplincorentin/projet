4048960bb304ab25147e99d2394fbb2badda79d4
Corentin <taplincorentin@gmail.com>
2023-12-15 11:34:23 +0100

    addFlash test

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -20,6 +20,8 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
+        $this->addFlash('notice', "You are now logged in !");
+
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
